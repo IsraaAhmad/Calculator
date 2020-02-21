572cf895e18348b2c273a1749246d4d54f803d84
User <User@DESKTOP-IBGB2Q9.mynet>
2020-02-21 21:48:14 +0200

    first pro

---
+++ Calcolator/src/Calcolator.java
@@ -0,0 +1,13 @@
+
+public class Calcolator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AddNumber(3,4);
+
+	}
+	public static  int AddNumber(int a, int b) {
+		return a+b;
+	}
+
+}
